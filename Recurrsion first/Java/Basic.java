ee13748231c9b90ce2fa102b73cb110d5b2ee86c
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-04-03 00:46:54 +0530

    basics

--- Recurrsion first/Java/Basic.java
+++ Recurrsion first/Java/Basic.java
@@ -2,10 +2,16 @@ package Java;
 
 public class Basic{
     public static void main(String[] args){
-        message();
+        int count = 0;
+        message(count);
     }
 
-    static void message(){
+    static void message(int count){
         System.out.println("Hello recursion");
+        if(count < 2){
+            count = count + 1;
+            message(count);
+        }
+
     }
 }
