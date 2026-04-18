8124ada2f527d0bfdb4de0529b719bcb9c1e772f
Prakhar Chaudhary <prakharchaudhary0302@gmail.com>
2026-04-18 15:56:16 +0530

    subset

--- Recurrsion first/Cpp/subset.cpp
+++ Recurrsion first/Cpp/subset.cpp
@@ -1,19 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-vector<int>subset(vector<int>&vec,int i,vector<int>ans){
-    ans.push_back(vec[i]);
-    subset(vec,i+1,ans);
-    ans.pop_back();
-    subset(vec,i+1,ans);
+void subset(vector<int>& vec, int i, vector<int>& temp, vector<vector<int>>& ans) {
+    // Base case
+    if (i == vec.size()) {
+        ans.push_back(temp);
+        return;
+    }
+
+    // Include current elements
+    temp.push_back(vec[i]);
+    subset(vec, i + 1, temp, ans);
+
+    // Exclude current element
+    temp.pop_back();
+    subset(vec, i + 1, temp, ans);
 }
 
-int main(){
-    vector<int>vec1 = {1,2,3,4,5};
-    vector<vector<int>>ans;
-    int i = 0;
-    
-    cout<<subset(vec1,i,ans);
+int main() {
+    vector<int> vec1 = {1, 2, 3};
+    vector<vector<int>> ans;
+    vector<int> temp;
 
+    subset(vec1, 0, temp, ans);
 
+    for (int i = 0; i < ans.size(); i++) {        // loop over all subsets
+    cout << "{ ";
+    for (int j = 0; j < ans[i].size(); j++) { // loop inside each subset
+        cout << ans[i][j] << " ";
+    }
+    cout << "}" << endl;
+}
+
+    return 0;
 }
